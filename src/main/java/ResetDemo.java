3f121391777b1d85d2ac7dd1d1ffb147ddb9e8e1
Bepus <tentieva869@gmail.com>
2025-07-13 17:01:05 +0600

    Reser version 2.2.1

--- src/main/java/ResetDemo.java
+++ src/main/java/ResetDemo.java
@@ -6,5 +6,6 @@ public class ResetDemo {
         System.out.println("Reser version 2.0.0 --> Hello world");
         System.out.println("Reser version 2.1.0 --> mixed");
         System.out.println("Reser version 2.1.1 --> mixed demo жаман задрал кылат экен");
+        System.out.println("Reser version 2.2.1");
     }
 }
