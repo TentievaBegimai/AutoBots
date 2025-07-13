cc1165bd7580ee9e21fa9a531669fa0d71350c3f
Bepus <tentieva869@gmail.com>
2025-07-13 12:02:59 +0600

    POPA-3.0-lol

--- src/main/java/com/autobots/Popa_3_LOL.java
+++ src/main/java/com/autobots/Popa_3_LOL.java
@@ -1,7 +1,6 @@
 package com.autobots;
 
 public class Popa_3_LOL {
-    public static void main(String[] args) {
-        hdhsdsjfowamckl mklc amcd;
+    public static void main(String[] args) {g
     }
 }
